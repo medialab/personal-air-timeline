a02a4c7716440adc5eaaa48138ef997190a2d83b
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-02-20 18:32:18 +0100

    Crunch timelines + sensor data

--- app/view_upload/upload.js
+++ app/view_upload/upload.js
@@ -29,16 +29,17 @@ angular.module('saveourair.view_upload', ['ngRoute'])
   function updateUploads() {
     var someTimelineFiles = Object.keys($scope.timelineFiles).length > 0
     var someSensorFiles = Object.keys($scope.sensorFiles).length > 0
+    var data = {}
     if (someSensorFiles && someTimelineFiles) {
       $scope.uploadStatusMessage = 'Sensor data .............. OK\nTimeline data ............ OK\n>>>>>>>>>>>>>>>>>> DATA READY\n\nNote: You may upload\n      additional files'
+      data = reconcileFiles($scope.sensorFiles, $scope.timelineFiles)
     } else if (someSensorFiles && !someTimelineFiles) {
       $scope.uploadStatusMessage = 'Sensor data .............. OK\nTimeline data > PLEASE UPLOAD'
     } else if (!someSensorFiles && someTimelineFiles) {
       $scope.uploadStatusMessage = 'Sensor data >>> PLEASE UPLOAD\nTimeline data ............ OK'
     }
 
-    store.set('timelineFiles', $scope.timelineFiles)
-    store.set('sensorFiles', $scope.sensorFiles)
+    store.set('data', data)
   }
 
   // File loading interactions
@@ -231,6 +232,194 @@ angular.module('saveourair.view_upload', ['ngRoute'])
     })
 
     return datapoints
+  }
+
+  function reconcileFiles(sensorfiles, timelinefiles) {
+    var fileName
+
+    /// Build spatial index
+    // Order and clean original intervals
+    var intervalsIndex = {}
+    var eventsList = []
+    for (fileName in timelinefiles) {
+      var data = timelinefiles[fileName]
+      data.forEach(function(d, i){
+        var intervalId = fileName + '_' + i
+        var interval = d
+        interval.id = intervalId
+        interval.beginTimestamp = Date.parse(d.begin)
+        interval.endTimestamp = Date.parse(d.end)
+        intervalsIndex[intervalId] = interval
+        eventsList.push({type:'begin', ts:interval.beginTimestamp, interval:intervalId})
+        eventsList.push({type:'end', ts:interval.endTimestamp, interval:intervalId})
+      })
+      eventsList.sort(function(a, b){ return (a.ts==b.ts) ? ( (a.type > b.type)?(-1):(1) ) : (a.ts - b.ts) })
+      // Filter to ensure a strict order event
+      var ignoredIntervals = {}
+      var lastEvent
+      eventsList.forEach(function(e){
+        if (ignoredIntervals[e.interval]) { return }
+        if (lastEvent !== undefined) {
+          if (e.type=='begin') {
+            if (lastEvent.type == 'end' && lastEvent.ts <= e.ts) {
+              // All is well
+              lastEvent = e
+              return
+            } else {
+              // Ignore
+              ignoredIntervals[e.interval] = true
+              console.error('[error] Timeline interval', e.interval, 'ignored for discrepancy reasons')
+              return
+            }
+          } else {
+            if (e.type=='end' && lastEvent.type=='begin' && e.interval==lastEvent.interval) {
+              // All is well
+              lastEvent = e
+              return
+            } else {
+              // Ignore
+              ignoredIntervals[e.interval] = true
+              console.error('[error] Timeline interval', e.interval, 'ignored for discrepancy reasons')
+              return
+            }
+          }
+        }
+      })
+      eventsList = eventsList.filter(function(e){
+        return ignoredIntervals[e.interval] == undefined
+      })
+      // console.log(eventsList)
+      // Build timestamps list
+      var timestampsIndex = {}
+      // Now we compute the path points of the intervals
+      eventsList.forEach(function(e){
+        // We get the intervals by looking only at the "begin" events
+        if (e.type == 'begin') {
+          var interval = intervalsIndex[e.interval]
+
+          if (interval.type == 'point') {
+            // Interval is a point
+            timestampsIndex[interval.beginTimestamp] = timestampsIndex[interval.beginTimestamp] || {ts:interval.beginTimestamp}
+            timestampsIndex[interval.beginTimestamp].after = interval.path.split(',').map(function(d){return +d})
+            
+            timestampsIndex[interval.endTimestamp] = timestampsIndex[interval.endTimestamp] || {ts:interval.endTimestamp}
+            timestampsIndex[interval.endTimestamp].before = interval.path.split(',').map(function(d){return +d})
+          } else {
+            // Interval is a linestring
+            // console.log(interval)
+            var coordinatesList = interval.path.split(' ')
+              .map(function(path){ return path.split(',').map(function(d){return +d}) })
+              .filter(function(path){ return path.length >= 2 })
+            var cumulativeDistances = [0]
+            var i
+            for (i=1; i<coordinatesList.length; i++) {
+              var d = Math.sqrt(Math.pow(coordinatesList[i][0] - coordinatesList[i-1][0], 2) + Math.pow(coordinatesList[i][1] - coordinatesList[i-1][1], 2))
+              if (isNaN(d)) {
+                console.error('[error] A distance for interval', interval.id, 'is not a number')
+                cumulativeDistances.push(cumulativeDistances[i-1])
+              } else {
+                cumulativeDistances.push(cumulativeDistances[i-1] + d)
+              }
+            }
+            var totalDistance = cumulativeDistances[cumulativeDistances.length-1]
+
+            if (totalDistance == 0) {
+              // It's actually a point!
+              timestampsIndex[interval.beginTimestamp] = timestampsIndex[interval.beginTimestamp] || {ts:interval.beginTimestamp}
+              timestampsIndex[interval.beginTimestamp].after = interval.path.split(',').map(function(d){return +d})
+              
+              timestampsIndex[interval.endTimestamp] = timestampsIndex[interval.endTimestamp] || {ts:interval.endTimestamp}
+              timestampsIndex[interval.endTimestamp].before = interval.path.split(',').map(function(d){return +d})
+            } else {
+              var ratios = cumulativeDistances.map(function(d){ return d/totalDistance })
+
+              timestampsIndex[interval.beginTimestamp] = timestampsIndex[interval.beginTimestamp] || {ts:interval.beginTimestamp}
+              timestampsIndex[interval.beginTimestamp].after = interval.path.split(',').map(function(d){return +d})
+              
+              coordinatesList.forEach(function(c, i){
+                var ratio = ratios[i]
+                var ts = interval.beginTimestamp + ratio * (interval.endTimestamp - interval.beginTimestamp)
+
+                timestampsIndex[ts] = timestampsIndex[ts] || {ts:ts}
+                timestampsIndex[ts].before = c
+                timestampsIndex[ts].after = c
+              })
+
+              timestampsIndex[interval.endTimestamp] = timestampsIndex[interval.endTimestamp] || {ts:interval.endTimestamp}
+              timestampsIndex[interval.endTimestamp].before = interval.path.split(',').map(function(d){return +d})
+            }
+
+          }
+
+        }
+      })
+
+      var timestamps = Object.values(timestampsIndex).sort(function(a, b) {return a.ts-b.ts})
+      var whereWasIAt = function(ts) {
+        if (ts < timestamps[0].ts || ts>timestamps[timestamps.length - 1].ts) {
+          return undefined
+        } else {
+          var i=0
+          for (i=0; i<timestamps.length; i++) {
+            if (timestamps[i].ts >= ts) {
+              break
+            }
+          }
+          if (timestamps[i-1].after && timestamps[i].before) {
+            var ratio = (ts - timestamps[i-1].ts) / (timestamps[i].ts - timestamps[i-1].ts)
+            var x1 = timestamps[i-1].after[0]
+            var y1 = timestamps[i-1].after[1]
+            var x2 = timestamps[i].before[0]
+            var y2 = timestamps[i].before[1]
+            return [x1 + ratio * (x2-x1), y1 + ratio * (y2-y1)]
+          } else if (timestamps[i-1].after) {
+            console.warn('Discrepancy',timestamps[i-1].after, timestamps[i].before)
+            return timestamps[i-1].after
+          } else if (timestamps[i].before) {
+            console.warn('Discrepancy',timestamps[i-1].after, timestamps[i].before)
+            return timestamps[i].before
+          } else {
+            return undefined
+          }
+        }
+      }
+    }
+
+    // Build data points after sensor data
+    var datapoints = {}
+    for (fileName in sensorfiles) {
+      var data = sensorfiles[fileName]
+      var dateFormatRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{4}) (\d{1,2}):(\d{2}):(\d{2})$/
+      data.forEach(function(d){
+
+        // Parse date
+        var dateArray = dateFormatRegex.exec(d[0] + ' ' + d[1])
+        var dateObject = new Date(
+            (+dateArray[3]),
+            (+dateArray[2])-1, // Careful, month starts at 0!
+            (+dateArray[1]),
+            (+dateArray[4]),
+            (+dateArray[5]),
+            (+dateArray[6])
+        )
+
+        var datestring = dateObject.toString()
+        var timestamp = dateObject.getTime()
+
+        var coordinates = whereWasIAt(timestamp)
+        datapoints[timestamp] = {
+          timestamp: timestamp,
+          datestring: datestring,
+          T: d[2],
+          RH: d[3],
+          P: d[4],
+          PM25: d[5],
+          PM10: d[6],
+          coordinates: coordinates
+        }
+      })
+    }
+    return Object.values(datapoints)
   }
 
   function parseXml(xmlStr) {
