b6e294af08e6ec3674e05f9f9139869c48676bb4
Yomguithereal <guillaumeplique@gmail.com>
2018-02-21 16:08:05 +0100

    Working leaflet markers

--- app/directives/leaflet.js
+++ app/directives/leaflet.js
@@ -5,20 +5,63 @@ angular.module('saveourair.directives.leaflet', []).directive('leaflet', [functi
     restrict: 'E',
     templateUrl: './directives/leaflet.html',
     scope: {
-
+      data: '='
     },
     link: function($scope, el, attrs) {
       var div = el.find('div')[0];
+
+      // Finding boundaries
+      var minX = Infinity,
+          maxX = -Infinity,
+          minY = Infinity,
+          maxY = -Infinity;
+
+      $scope.data.forEach(function(d) {
+        if (d.x < minX)
+          minX = d.x;
+        if (d.x > maxX)
+          maxX = d.x;
+
+        if (d.y < minY)
+          minY = d.y;
+        if (d.y > maxY)
+          maxY = d.y;
+      });
 
       $scope.map = Leaflet.map(div, {
         center: [56.056695, 9.841720],
         zoom: 6
       });
 
+      $scope.map.fitBounds([
+        [minY, minX],
+        [maxY, maxX]
+      ]);
+
       Leaflet.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
         attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 18,
       }).addTo($scope.map);
+
+      // Adding points
+      $scope.data.forEach(function(d) {
+        var marker = Leaflet.circleMarker([d.y, d.x], {
+          radius: 5,
+          fillOpacity: 1,
+          color: '#fff'
+        });
+
+        marker.addTo($scope.map);
+      });
+
+      $scope.data.forEach(function(d) {
+        var marker = Leaflet.circleMarker([d.y, d.x], {
+          radius: 3,
+          fillOpacity: 1
+        });
+
+        marker.addTo($scope.map);
+      });
     }
   };
 }]);
