0a9a205fbac1c386b76e3ff4d5d3eca207b21caa
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-02-21 15:02:53 +0100

    fix interpolation

--- app/view_board/board.js
+++ app/view_board/board.js
@@ -29,6 +29,8 @@ angular.module('saveourair.view_board', ['ngRoute'])
   			d.P = +d.P
   			d.PM25 = +d.PM25
   			d.PM10 = +d.PM10
+  			d.DCE_PM25 = +d.DCE_PM25 || undefined
+  			d.DCE_PM10 = +d.DCE_PM10 || undefined
   			d.x = +d.x || undefined
   			d.y = +d.y || undefined
         var defined = d_prev && d.timestamp - d_prev.timestamp < fiveminutesms
