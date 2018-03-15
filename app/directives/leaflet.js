af402f4f008432c6da3deaeacf8c70c59ff7b7cf
mikima <mikimauri+github@gmail.com>
2018-03-15 10:30:51 +0100

    Fixed conflict, built new app

--- app/directives/leaflet.js
+++ app/directives/leaflet.js
@@ -45,12 +45,8 @@ angular.module('saveourair.directives.leaflet', []).directive('leaflet', [functi
         [maxY, maxX]
       ]);
 
-<<<<<<< HEAD
+      //Leaflet.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
       Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mikima/cjdyiowio2v2c2sn31jhs4g9e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlraW1hIiwiYSI6IjNvWUMwaUEifQ.Za_-O03W3UdQxZwS3bLxtg', {
-=======
-      Leaflet.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-      //Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mikima/cjdyiowio2v2c2sn31jhs4g9e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlraW1hIiwiYSI6IjNvWUMwaUEifQ.Za_-O03W3UdQxZwS3bLxtg', {
->>>>>>> master
         attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 18,
       }).addTo($scope.map);
