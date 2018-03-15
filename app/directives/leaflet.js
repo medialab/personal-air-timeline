16e0ec5a93964921b14e4788003c1e624758e39b
mikima <mikimauri+github@gmail.com>
2018-03-15 10:19:16 +0100

    Added coloured version

--- app/directives/leaflet.js
+++ app/directives/leaflet.js
@@ -1,5 +1,7 @@
 var Leaflet = require('leaflet');
 
+console.log('leaflet.js')
+
 angular.module('saveourair.directives.leaflet', []).directive('leaflet', [function() {
   return {
     restrict: 'E',
@@ -44,6 +46,7 @@ angular.module('saveourair.directives.leaflet', []).directive('leaflet', [functi
       ]);
 
       Leaflet.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+      //Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mikima/cjdyiowio2v2c2sn31jhs4g9e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlraW1hIiwiYSI6IjNvWUMwaUEifQ.Za_-O03W3UdQxZwS3bLxtg', {
         attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 18,
       }).addTo($scope.map);
